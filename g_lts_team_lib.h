5efe67e571d09bac614f82b02c99f69e9a34009e
Tas-sos <tasosnumberten@yahoo.gr>
2015-03-25 15:46:49 +0200

    Έτοιμο για το πρώτο δείγμα - μη κατευθυνόμενος γράφος.

--- g_lts_team_lib.h
+++ g_lts_team_lib.h
@@ -38,7 +38,7 @@ public :
 	for ( unsigned int node = 0; node < grafima.size(); node++ )
 		if ( grafima[node].name == onoma_korifis ) /* Αν βρει πουθενά το ίδιο όνομα σε κάποιο κόμβο - κορυφή του γράφου. */
 			{
-			cout << "Το όνομα υπάρχει." << endl;
+			//cout << "Το όνομα υπάρχει." << endl;
 			return 0; /* Βγες με false. - Δε το δεχόμαστε ρε. - */
 			}
 	/* Τέλος ελέγχου. */
@@ -64,6 +64,7 @@ public :
 		if ( grafima[node].name == kapia_Korifi ) /* Αν βρει την κορυφή που θέλει να συνδέσει. */
 			{
 			grafima[node].sindeete_me.push_back(na_sindethi_me_tin_korifi);
+			eisagogi_korifis(na_sindethi_me_tin_korifi); /* Βάζουμε και την κορυφή αυτή ( αν δεν υπάρχει ) στο γράφο. */
 			return 1; 
 			}
 	/* Τέλος ελέγχου. */
@@ -83,6 +84,10 @@ public :
 	{
 	for ( unsigned int node = 0; node < grafima.size(); node++ )
 		{
+		if ( grafima[node].sindeete_me.empty() ) /* Αν ο κόμβος - κορυφή δεν συνδέετε με καμία άλλη κορυφή - κόμβο. */
+			continue; /* Τότε δεν θα τον εμφανίσεις..απλώς συνέχισε με την επόμενη εκτέλεση της λούπας
+					δηλαδή με τον επόμενο κόμβο. ;) */
+			
 		cout << "Ο κόμβος " << grafima[node].name << " συνδέετε με την/τις κορυφές : " ;
 		
 		for ( unsigned int j = 0; j < grafima[node].sindeete_me.size(); j++ )
@@ -97,33 +102,46 @@ public :
 	}
 	
 	
+
+
+/*
+Παράδειγμα για το edges_table.csv αρχείο :
+	Source,	Target,	Weight,	Label,	Type
+	1,	3,	10,	"1->3",	Directed
 	
+Παράδειγμα για το nodes_table.csv αρχείο :	
+	ID  , Label
+	1   , nod1
+*/
 	
-	void write_to_file(string filename = "graph.txt")
+	void write_to_file(string edges_filename = "edges_table.csv", string nodes_filename = "nodes_table.csv")
 	{
-	ofstream fp;
-	fp.open (filename);
+	ofstream edges_fl;
+	ofstream nodes_fl;
 	
+	edges_fl.open(edges_filename);
+	nodes_fl.open(nodes_filename);
+	
+	edges_fl << "Source,Target,Weight,Label,Type" << endl;
+	nodes_fl << "ID,Label" << endl;
 	
 	for ( unsigned int node = 0; node < grafima.size(); node++ )
 		{
-		fp << grafima[node].name << ", " ;
-		for ( unsigned int j = 0; j < grafima[node].sindeete_me.size(); j++ )
-			
-			if ( j == (grafima[node].sindeete_me.size()-1) ) /* Αν έχει φτάσει στο τέλος τις λίστας να βάλει . και όχι , .*/
-				fp << grafima[node].sindeete_me[j] << ".";
-			else
-				fp << grafima[node].sindeete_me[j] << ", ";
+		
+		/*  */
+		nodes_fl << grafima[node].name << "," << "Κορυφή" << grafima[node].name << endl;
 
-		fp << endl;
+
+		for ( unsigned int j = 0; j < grafima[node].sindeete_me.size(); j++ )
+				{ 
+				/* Δίνω μόνο source, target και type τα υπόλοιπα τα αφήνω κενά. */
+				edges_fl << grafima[node].name << "," << grafima[node].sindeete_me[j] << ",,,Undirected" << endl;
+				}
 		}
 	
 	
-	fp << "Writing By G-lts Team.\n";
-	
-	
-	
-	fp.close();
+	edges_fl.close();
+	nodes_fl.close();
 	}
 	
 	
